a1394d0469b82e0021bdcaa4a72bf2245c0e075a
ilias <iliasamchichou@gmail.com>
2023-10-28 18:49:25 +0200

    a

--- Prueba_buscar.py
+++ Prueba_buscar.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 # Inicializa el navegador y abre WhatsApp Web
@@ -10,7 +12,7 @@ driver.get("https://web.whatsapp.com/")
 input("Escanea el código QR y presiona Enter cuando esté listo...")
 
 # Nombre del contacto al que deseas enviar un mensaje
-contact_name = "Ilias"
+contact_name = "Pablo"
 
 # Espera para asegurarse de que WhatsApp Web esté completamente cargado
 time.sleep(5)
@@ -26,4 +28,17 @@ for contact_element in contact_elements:
         contact_element.click()
         time.sleep(2)  # Espera 2 segundos para que la estructura de la página cambie
         break  # Detener el bucle cuando se encuentra el contacto
+wait = WebDriverWait(driver, 10)  # Establece un tiempo máximo de espera de 10 segundos
+input_box = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div/p/span')))
+
+# Cantidad de veces que deseas enviar el mensaje "hola"
+veces_a_enviar = 2
+
+for _ in range(veces_a_enviar):
+    # Escribe el mensaje "hola"
+    input_box.send_keys("hola")
+    # Envía el mensaje presionando Enter (opcional)
+    input_box.send_keys("\n")
+    time.sleep(0.5)
+
 input("Dale enter para cerrar la ventana")
