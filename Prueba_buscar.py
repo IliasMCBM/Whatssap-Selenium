15362c7fc44ca8bf938cb5af202c4a7fabb2d5e8
ilias <iliasamchichou@gmail.com>
2023-10-28 17:45:01 +0200

    a

---
+++ Prueba_buscar.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.wait import WebDriverWait
+import time
+
+'''# Opciones de Chrome para ejecutar en modo headless
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')'''
+
+# Inicializa el navegador y abre WhatsApp Web
+driver = webdriver.Chrome()
+driver.get("https://web.whatsapp.com/")
+
+# Escanea el código QR manualmente
+input("Escanea el código QR y presiona Enter cuando esté listo...")
+
+# Nombre del contacto al que deseas enviar un mensaje
+contact_name = "Nizar"
+
+# Espera para asegurarse de que WhatsApp Web esté completamente cargado
+time.sleep(5)
+
+# Encuentra todos los elementos de contacto
+contact_elements = driver.find_elements_by_css_selector('.lhggkp7q.ln8gz9je.rx9719la')
+
+# Recorre los elementos de contacto para encontrar "Nizar"
+for contact_element in contact_elements:
+    contact_text = contact_element.text
+    if contact_name in contact_text:
+        contact_element.click()
+        break  # Detener el bucle cuando se encuentra el contacto
+
+# Ahora debería estar abierto el chat con "Nizar"
+
+# Cierra el navegador
+driver.quit()
