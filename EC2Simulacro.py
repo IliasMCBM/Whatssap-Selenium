b129a8674b2295088adcf33192a25966a3500593
Tormenta88 <tormenta8282@gmail.com>
2023-10-31 16:56:27 +0100

    foto2

--- EC2Simulacro.py
+++ EC2Simulacro.py
@@ -21,8 +21,9 @@ driver.get("https://web.whatsapp.com/")
 input("Presiona enter para hacer la captura")
 driver.save_screenshot("./screenshot.png")
 input("Presiona enter cuando hayas scaneado el codigo")
+driver.save_screenshot("./screenshot2.png")
 
-contact_name = input("Dame el nombre del contacto")
+contact_name = str( input("Dame el nombre del contacto"))
 
 # Encuentra todos los elementos de contacto por su clase
 contact_elements = driver.find_elements(By.CLASS_NAME, 'Mk0Bp')
