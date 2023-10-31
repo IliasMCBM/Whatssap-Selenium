f9d349b16389192eb8365b9d232eb1b805795d1c
ilias <iliasamchichou@gmail.com>
2023-10-31 21:25:02 +0100

    a

--- EC2Simulacro.py
+++ EC2Simulacro.py
@@ -10,10 +10,6 @@ from selenium.webdriver.common.keys import Keys
 import schedule
 
 
-
-
-
-
 # Configura las opciones de Firefox
 firefox_options = Options()
 firefox_options.add_argument("--headless")
@@ -39,6 +35,7 @@ input('Avisa cuando estes listo')
 # Cierra Firefox
 driver.quit()
 
+# Configura el nombre del contacto
 contact_name = str(input("Dame el nombre del contacto"))
 
 # Encuentra todos los elementos de contacto por su clase
@@ -46,15 +43,18 @@ contact_elements = driver.find_elements(By.CLASS_NAME, 'Mk0Bp')
 # Filtra los elementos que tienen la segunda clase " _30scZ"
 contact_elements = [element for element in contact_elements if " _30scZ" in element.get_attribute("class")]
 
+# Encuentra el elemento de contacto con el nombre especificado
 for contact_element in contact_elements:
     contact_text = contact_element.text
     if contact_name in contact_text:
         contact_element.click()
         time.sleep(2)  # Espera 2 segundos para que la estructura de la página cambie
-        break  # Detener el bucle cuando se encuentra el contacto
-time.sleep(1.5)
+        break
+
+# Configura el mensaje a enviar
 mensaje = input("Dime cual va a ser el mensaje a enviar")
 
+# Envía el mensaje 10 veces
 for i in range(10):
     wait = WebDriverWait(driver, 10)  # Establece un tiempo máximo de espera de 10 segundos
     input_box = wait.until(EC.presence_of_element_located(
@@ -66,13 +66,16 @@ for i in range(10):
 
 # Envía el mensaje presionando Enter (opcional)
     input_box.send_keys(Keys.RETURN)
-input('Cerrar')
-driver.close()
+
+# Cierra Firefox
+driver.quit()
+
+# Imprime un mensaje de confirmación
 print("Mensajes enviados")
 
 
 # Programa la ejecución del código a las 14:00
-
+#chedule.every().day.at("14:00").do(lambda: driver.get("https://web.whatsapp.com/"))
 
 # Ejecuta el programa en segundo plano
 while True:
