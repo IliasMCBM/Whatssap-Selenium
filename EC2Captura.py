de9cf56ca29c89e482a91a0beba303b2858df111
Tormenta88 <tormenta8282@gmail.com>
2023-10-31 13:22:12 +0100

    Firefox-Google

--- EC2Captura.py
+++ EC2Captura.py
@@ -1,20 +1,22 @@
 import pandas as pd
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.wait import WebDriverWait
 import time
 
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome(options=chrome_options)
+firefox_options = Options()
+firefox_options.add_argument("--headless")
+firefox_options.add_argument('--no-sandbox')
+firefox_options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Firefox(options=firefox_options)
 # Inicializa el navegador y abre WhatsApp Web
 driver.get("https://web.whatsapp.com/")
 # Escanea el código QR manualmente
 input("Presiona enter para hacer la captura")
 driver.save_screenshot("./screenshot.png")
+
+
